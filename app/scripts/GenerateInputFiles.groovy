288643e38bf2b29437e4ca680b5ac7ddb25089a6
3rickDJ <erick@makingdevs.com>
2024-08-23 21:41:12 -0600

    add: script to generate list of processes

---
+++ app/scripts/GenerateInputFiles.groovy
@@ -0,0 +1,23 @@
+// Definir el directorio donde se crearán los archivos
+def inputDir = new File("${project.buildDir}/inputFiles")
+
+// Crear el directorio si no existe
+if (!inputDir.exists()) {
+    inputDir.mkdirs()
+}
+
+def seconds = 1..20
+def kb_size = 2..1000
+def lista = (1..300).collect {
+    def s = seconds.shuffled().first()
+    def k = kb_size.shuffled().first()
+    "${it},${s},${k}"
+}
+
+def fileContent = lista.join('\n')
+
+// Crear y escribir en el archivo
+def inputFile = new File(inputDir, "listaProcesos.csv")
+inputFile.text = fileContent
+
+println "Archivo de entrada creado en: ${inputFile.absolutePath}"
